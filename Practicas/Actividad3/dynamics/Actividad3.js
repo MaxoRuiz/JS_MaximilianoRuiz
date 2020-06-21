c977efa63d565012483c77e1aeb2bece9fd9eea6
unknown <maxruizg@comunidad.unam.mx>
2020-06-21 01:04:50 -0500

    FEAT:Práctica 3

---
+++ Practicas/Actividad3/dynamics/Actividad3.js
@@ -0,0 +1,11 @@
+//La matriz para la serie de Fibonacci
+var Caracolin=[
+  [0,1,1,2],
+  [89,144,233,3],
+  [55,987,610,5],
+  [34,21,13,8]
+]
+//Se transforma la matriz en un string para mantener su estructura
+Caracolin.toString();
+//Se sube a la consola
+console.log(Caracolin);
